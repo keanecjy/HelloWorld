8001dbfcd23df209eecd626496300300830719d6
JinHao-L <limjinhao@gmail.com>
2021-01-08 16:53:25 +0800

    Add google maps and cluster feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,79 +1,96 @@
+import React, {useState, useEffect} from 'react';
 import logo from "./HelloWorldLogo.svg";
 import "./App.css";
+import {io} from "socket.io-client";
+import GoogleMap from "./components/GoogleMap";
+import GoogleMapReact from "google-map-react";
+import {defaultStartCoords, fakeUsers} from "./util/fakeUsers";
+
+const SERVER_URL = "http://localhost:5000";
+
 // const { io } = window["socket.io-3.0.5.min"];
 
 function App() {
-  console.log(window);
-  // const socket = io("http://localhost:5000", {
-  //   withCredentials: true,
-  //   extraHeaders: {
-  //     "my-custom-header": "abcd",
-  //   },
-  // });
+    console.log(window);
+    // const socket = io("http://localhost:5000", {
+    //   withCredentials: true,
+    //   extraHeaders: {
+    //     "my-custom-header": "abcd",
+    //   },
+    // });
 
-  // socket.on("connected", (message) => {
-  //   console.log("user connected");
-  // });
+    // socket.on("connected", (message) => {
+    //   console.log("user connected");
+    // });
 
-  // socket.on("status", (msg) => {
-  //   console.log(msg);
-  // });
+    // socket.on("status", (msg) => {
+    //   console.log(msg);
+    // });
 
-  // socket.emit("inputUser", {
-  //   username: "test",
-  //   lat: "0000",
-  //   long: "20000",
-  // });
+    // socket.emit("inputUser", {
+    //   username: "test",
+    //   lat: "0000",
+    //   long: "20000",
+    // });
 
-  // socket.on("outputUser", (users) =>
-  //   users.map((user) => console.log("user " + user.username + " joined"))
-  // );
+    // socket.on("outputUser", (users) =>
+    //   users.map((user) => console.log("user " + user.username + " joined"))
+    // );
 
-  // socket.emit("inputMessage", { username: "", text: "Hello to you too!" });
+    // socket.emit("inputMessage", { username: "", text: "Hello to you too!" });
 
-  // socket.emit("inputMessage", { username: "Dhafin", text: "" });
+    // socket.emit("inputMessage", { username: "Dhafin", text: "" });
 
-  // socket.emit("inputMessage", { username: "Frank", text: "Hello!" });
+    // socket.emit("inputMessage", { username: "Frank", text: "Hello!" });
 
-  // socket.emit("inputMessage", {
-  //   username: "Dhafin",
-  //   text: "Hello to you too!",
-  // });
+    // socket.emit("inputMessage", {
+    //   username: "Dhafin",
+    //   text: "Hello to you too!",
+    // });
 
-  // socket.emit("inputPosition", {
-  //   username: "test",
-  //   lat: "1200",
-  //   long: "3000",
-  // });
+    // socket.emit("inputPosition", {
+    //   username: "test",
+    //   lat: "1200",
+    //   long: "3000",
+    // });
 
-  // socket.on("outputPosition", (users) => {
-  //   users.map((user) =>
-  //     console.log(
-  //       "user " + user.username + " moved to " + user.long + " " + user.lat
-  //     )
-  //   );
-  // });
+    // socket.on("outputPosition", (users) => {
+    //   users.map((user) =>
+    //     console.log(
+    //       "user " + user.username + " moved to " + user.long + " " + user.lat
+    //     )
+    //   );
+    // });
 
-  // socket.on("outputMessage", (messages) => {
-  //   messages.map((message) => console.log(message.text));
-  // });
+    // socket.on("outputMessage", (messages) => {
+    //   messages.map((message) => console.log(message.text));
+    // });
+    const currLocation = {lat: 1.3521, lng: 103.8198}
+    // socket.on("userLeft", (userId) => console.log("user " + userId + " left"));
+    const [coordinates, setCoordinates] = useState(currLocation);
+    // useEffect((center) => {
+    //     setCoordinates(center);
+    // });
 
-  // socket.on("userLeft", (userId) => console.log("user " + userId + " left"));
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <br />
-        <p>
-          <strong> Welcome to HelloWorld </strong>
-        </p>
-        <p>
-          <strong> The world's most engaging map </strong>
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/*<header className="App-header">*/}
+            {/*    <img src={logo} className="App-logo" alt="logo"/>*/}
+            {/*    <br/>*/}
+            {/*    <p>*/}
+            {/*        <strong> Welcome to HelloWorld </strong>*/}
+            {/*    </p>*/}
+            {/*    <p>*/}
+            {/*        <strong> The world's most engaging map </strong>*/}
+            {/*    </p>*/}
+            {/*</header>*/}
+            <GoogleMap center={coordinates} zoom={15}/>
+            <button className={"recenter-button"} onClick={() => {
+                console.log("CLICKED");
+                setCoordinates(currLocation)
+            }}>RE-CENTER</button>
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/util/fakeUsers.js
@@ -0,0 +1,27 @@
+const TOTAL_USERS_COUNT = 200;
+
+export const defaultStartCoords = {lat: 1.3521, lng: 103.8198};
+
+export const fakeUsers = [...Array(TOTAL_USERS_COUNT)]
+    .fill(0)
+    .map((val, index) => ({
+        id: index,
+        lat: defaultStartCoords.lat +
+            0.2 *
+            index *
+            Math.sin(Math.PI * index / 180) *
+            Math.cos(23 * Math.PI * index / 180) +
+            Math.sin(index / 180),
+        lng: defaultStartCoords.lng +
+            0.2 *
+            index *
+            Math.cos(17 * Math.PI * index / 180) *
+            Math.sin(5 * Math.PI * index / 180) +
+            Math.sin(index / 180),
+        details: {
+            chatMessage: "HI",
+            // random color
+            iconColor: "#" + Math.floor(Math.random()*16777215).toString(16),
+            name: "User" + index.toString(10)
+        }
+    }));
