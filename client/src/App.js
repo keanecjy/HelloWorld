7ac231ebb6568a490115dcd9edc1efe088b34b79
keanecjy <e0425601@u.nus.edu>
2021-01-08 21:00:59 +0800

    Fix merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,3 @@
-import React, { useState, useEffect } from "react";
-import logo from "./HelloWorldLogo.svg";
-import "./App.css";
-import io from "socket.io-client";
-import GoogleMap from "./components/GoogleMap";
-import GoogleMapReact from "google-map-react";
-import { defaultStartCoords, fakeUsers } from "./util/fakeUsers";
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import io from 'socket.io-client';
@@ -17,66 +10,68 @@ export const StateContext = React.createContext({});
 
 function App() {
   useEffect(() => {
-    const socket = io("http://localhost:5000", {
+    const socket = io('http://localhost:5000', {
       withCredentials: true,
       extraHeaders: {
-        "my-custom-header": "abcd",
+        'my-custom-header': 'abcd',
       },
     });
 
-    socket.on("connect", (message) => {
-      console.log("A new user has connected");
+    socket.on('connect', (message) => {
+      console.log('A new user has connected');
     });
 
-    socket.on("status", (msg) => {
+    socket.on('status', (msg) => {
       console.log(msg);
     });
 
-    socket.emit("inputUser", {
-      username: "test",
-      lat: "0000",
-      long: "20000",
-      avatar: "male1",
+    socket.emit('inputUser', {
+      username: 'test',
+      lat: '0000',
+      long: '20000',
+      avatar: 'male1',
     });
 
-    socket.on("outputUser", (users) =>
-      users.map((user) => console.log("user " + user.username + " joined"))
+    socket.on('outputUser', (users) =>
+      users.map((user) => console.log('user ' + user.username + ' joined'))
     );
 
-    socket.emit("inputMessage", {
-      text: "Hello to you too!",
+    socket.emit('inputMessage', {
+      text: 'Hello to you too!',
     });
 
-    socket.emit("inputPosition", {
-      lat: "1200",
-      long: "3000",
+    socket.emit('inputPosition', {
+      lat: '1200',
+      long: '3000',
     });
 
-    socket.on("outputPosition", (users) => {
+    socket.on('outputPosition', (users) => {
       users.map((user) =>
-        console.log(
-          "user " + user.username + " moved to " + user.long + " " + user.lat
-        )
+        console.log('user ' + user.username + ' moved to ' + user.long + ' ' + user.lat)
       );
     });
 
-    socket.on("onlineUsers", (number) => {
-      console.log("users: " + number);
+    socket.on('onlineUsers', (number) => {
+      console.log('users: ' + number);
     });
 
-    socket.on("outputMessage", (messages) => {
+    socket.on('outputMessage', (messages) => {
       messages.map((message) => console.log(message.text));
     });
 
-    socket.on("userLeft", (userId) => console.log("user " + userId + " left"));
+    socket.on('userLeft', (userId) => console.log('user ' + userId + ' left'));
+
+    const isInitialized = window.localStorage.getItem('initialized');
+    if (isInitialized) {
+      setScreen(false);
+    }
   }, []);
 
+  // Coordinates
   const currLocation = { lat: 1.3521, lng: 103.8198 };
   const [coordinates, setCoordinates] = useState(currLocation);
-  const currLocation = { lat: 1.3521, lng: 103.8198 };
 
   // Global variables
-  const [coordinates, setCoordinates] = useState(currLocation);
   const [initialScreen, setScreen] = useState(true);
   const [name, setName] = useState('');
   const [image, setImage] = useState('');
@@ -87,13 +82,6 @@ function App() {
     image,
     setImage,
   };
-
-  useEffect(() => {
-    const isInitialized = window.localStorage.getItem('initialized');
-    if (isInitialized) {
-      setScreen(false);
-    }
-  }, []);
 
   return (
     <div className="App">
